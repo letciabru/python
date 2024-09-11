60bd68c6212e054bbcf99ded0d21dca33cab7f88
Brunna Letícia Ferreira da Silva <brunnalet26@gmail.com>
2024-09-11 14:18:19 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Brunna"
+idade = 16
+altura = 1.69
+
+# exibir mensagem na tela
+print("nome: ",nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string 
+print(f"nome: {nome} idade: {idade} altura: {altura}")
