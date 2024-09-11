fc26a82f317e41446743f45396de3107940ef667
Brunna Letícia Ferreira da Silva <brunnalet26@gmail.com>
2024-09-11 14:18:03 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,20 @@
+# aqui eu posso adicionar comentários 
+# continuar na linha de baixo
+print("Brunna") # comentário na linha do código
+print('Brunna')
+print("Brunna 'Bru' Letícia")
+print('Brunna "Bru" Letícia')
+print("Brunna \"Bru\" Letícia")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
