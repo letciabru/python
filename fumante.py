a739bb0a4dd99fe1fc325c55547a0ea033e47370
Brunna Letícia Ferreira da Silva <brunnalet26@gmail.com>
2024-09-25 16:56:14 -0300

    Add files via upload

---
+++ fumante.py
@@ -0,0 +1,17 @@
+# Este programa calcula a redução do tempo de vida de um fumante.
+
+dia = int(input("Quantos cigarros você fuma por dia? "))
+anos= int(input("Quantos anos você já fuma? "))
+
+# Calculando o número total de cigarros fumados.
+total = dia * 365 * anos
+
+# Calculando a redução total do tempo de vida em minutos.
+reducaominutos = total * 10
+
+
+# Calculando a redução total do tempo de vida em dias.
+reducaodias = reducaominutos // (24 * 60)
+
+# Exiba o total em dias.
+print("Você perderá", reducaodias, "dias de vida.")
