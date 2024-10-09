2091a75482b2d3450fc2c70295bbd154941efd71
Brunna Letícia Ferreira da Silva <brunnalet26@gmail.com>
2024-10-09 15:28:09 -0300

    Add files via upload

---
+++ break.py
@@ -0,0 +1,9 @@
+while True:
+    nome = input('digite seu nome ou escreva "sair": ')
+
+    if nome == 'sair':
+        break # para a execução do while, sai do loop de repetição
+
+    print(f'seu nome é  {nome}')
+
+print('acabou!!!')
---
+++ continue.py
@@ -0,0 +1,21 @@
+# ferramenta continue
+# a ferramenta continue no py vai interromper o loop,
+# mas vai dar continuidade a ele
+
+contador = 0
+
+while contador <= 40:
+    contador += 1
+
+    # verifica se o valor de 'contador' é multiplo de 4
+    if (contador % 4 == 0):
+        print("pim") # se for multiplo de 4, imprime "pim"
+        continue # interrompe a iteração atual e volta para o início do loop
+
+    # se o número não for multiplo de 4, imprime o valor do contador
+
+    print(contador)
+
+ # após o término do loop, imprime a mensagem de finalização
+
+print("fim  do programa")
